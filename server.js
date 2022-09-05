3785fc1fadf46d6fdad09bff9520ddda0e62e34e
Your Name <you@example.com>
2022-09-05 18:10:37 -0400

    modelfilescreated

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', async (req,res) => {
     res.send("WORKING")
