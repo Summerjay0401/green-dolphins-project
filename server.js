4d3a0ffa76b38d1d0ab73c57c6f142c6bec72322
Summer Jade <summerjadefrosal@gmail.com>
2022-09-06 10:17:02 +0800

    fix eslint errors

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const express = require("express");
+const express = require('express');
 // const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const sequilize = require("./config/connection");
-const { user } = require("./models");
+const sequilize = require('./config/connection');
+
+// const { user } = require('./models');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', async (req,res) => {
-    res.send("WORKING")
+  res.send('WORKING');
 });
 
 sequilize.sync({force: true}).then(() => {
-    app.listen(PORT, () => console.log("LISTENING"));
+  app.listen(PORT, () => console.log('LISTENING'));
 });
 
 
