ec9f228708594bc6e1714c711e9b5c00bb46ea4c
Your Name <you@example.com>
2022-09-08 19:25:22 -0400

    jsupdates

--- public/js/config.js
+++ public/js/config.js
@@ -4,5 +4,5 @@ const config = {
     CometChatAppId: '218378a179be2bf7',
     CometChatRegion: 'us',
     CometChatAuthKey: 'd89eb8f62c7e74e29f8189dc154b5e802123537f',
-    CometChatAPIKey: 'b7f559c02571feb81c525253bb23d468e1dd1146' 
+    CometChatAPIKey: 'b7f559c02571feb81c525253bb23d468e1dd1146'
   };
