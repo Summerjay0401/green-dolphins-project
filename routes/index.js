3d659b16128a58db4af31573bba544523527f071
Summer Jade <summerjadefrosal@gmail.com>
2022-09-12 14:21:56 +0800

    links

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,17 @@ const {
     homeRoutes
 } = require('./views');
 
+const {
+    loginView,
+    signUpView
+} = require('../controllers/user-controller');
+
 router.use('/', homeRoutes);
+router.use('/login', loginView);
+router.use('/signup', signUpView);
+router.get('*',function (req, res) {
+    res.redirect('/');
+});
+
 
 module.exports = router;
