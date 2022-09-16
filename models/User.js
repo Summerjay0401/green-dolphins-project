d658550eb3debbb38618912c2fa22c80e8a3bafe
Summer Jade <summerjadefrosal@gmail.com>
2022-09-16 22:39:54 +0800

    readded for correction

---
+++ models/User.js
@@ -0,0 +1,63 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+    bio: {
+      type: DataTypes.STRING
+    }
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    freezeTableName: true,
+    timestamps: true,
+    createdAt: true,
+    updatedAt: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
