5c3bbbbb3c4b1df99bcc585aaf27fa5bc5cefba5
Summer Jade <summerjadefrosal@gmail.com>
2022-09-17 21:23:49 +0800

    debug

--- utils/helpers.js
+++ utils/helpers.js
@@ -19,6 +19,7 @@ module.exports = {
         }
     },
     ifEquals: (arg1, arg2, options) => {
+        console.log(arg1, arg2);
         return (arg1 === arg2) ? options.fn(this) : options.inverse(this);
     }
 };
